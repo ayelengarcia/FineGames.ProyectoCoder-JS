be9502d6158cd172ffd895041ce3610b55ac5c8b
Ayelen Garcia <ayelengarcia7@hotmail.com>
2022-12-16 12:25:48 -0300

    vista btnCarsl

--- js/index.js
+++ js/index.js
@@ -27,17 +27,16 @@ fetch(URLjuegos)
   .then(() => recorrerObjetos(mostrarPorCategoria("Oferta"), returnJuego, contenidoSlide))
   .then(() => recorrerObjetos(mostrarPorCategoria("Gratuito"), returnJuego, contenidoSlide2))
   .then(() => recorrerObjetos(mostrarPorCategoria("Popular"), returnJuego, contenidoSlide3))
-  .then(() =>  recorrerBtnsCarrousel(btnCarousel, contenidoSlide))
-  .then(() =>  recorrerBtnsCarrousel(btnCarousel2, contenidoSlide2))
-  .then(() =>  recorrerBtnsCarrousel(btnCarousel3, contenidoSlide3))
+  .then(() => recorrerBtnsCarrousel(btnCarousel, contenidoSlide))
+  .then(() => recorrerBtnsCarrousel(btnCarousel2, contenidoSlide2))
+  .then(() => recorrerBtnsCarrousel(btnCarousel3, contenidoSlide3))
+  .then(() => vistaButtonsCarousel())
   .then(() => agregarAlCarrito(JUEGOS))
   .catch(error => contenedorJuegos.innerHTML = retornError())
 
 function mostrarPorCategoria(categoria) {
   let juegosCategoria = categoria;
-  let encontrado = JUEGOS.filter(
-    (juego) => juego.categoria === juegosCategoria
-  );
+  let encontrado = JUEGOS.filter((juego) => juego.categoria === juegosCategoria);
   return encontrado;
 }
 
@@ -50,22 +49,24 @@ function recorrerBtnsCarrousel(button, slideCarrousel) {
       slideCarrousel.style.transform = `translateX(${operacion}%)`;
       slideCarrousel.style.transition = "1s";
 
-      button.forEach((btn, i) => {
-        button[i].classList.remove("activo");
-      });
+      button.forEach((btn, i) => button[i].classList.remove("activo"));
       button[i].classList.add("activo");
     });
   });
 }
 
-window.onresize = function (event) {
+
+function vistaButtonsCarousel() {
   if (window.innerWidth > 768) {
-    btnCarouselOcultar.forEach((btn, i) => {
-      btnCarouselOcultar[i].classList.add("d-none");
-    });
-  } else {
-    btnCarouselOcultar.forEach((btn, i) => {
-      btnCarouselOcultar[i].classList.remove("d-none");
-    });
+    btnCarouselOcultar.forEach((btn, i) => btnCarouselOcultar[i].classList.add("d-none"));
+  } else if (window.innerWidth < 768) {
+    btnCarouselOcultar.forEach((btn, i) => btnCarouselOcultar[i].classList.remove("d-none"));
   }
-};
+  window.onresize = function (event) {
+    if (window.innerWidth > 768) {
+      btnCarouselOcultar.forEach((btn, i) => btnCarouselOcultar[i].classList.add("d-none"));
+    } else {
+      btnCarouselOcultar.forEach((btn, i) => btnCarouselOcultar[i].classList.remove("d-none"));
+    }
+  };
+}
